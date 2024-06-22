dd71665bda77d47a3f725fa3a80e62a6b50bd538
Isabel <isa.svalero@gmail.com>
2024-06-22 13:14:22 +0200

    color icono menu

--- script/script.js
+++ script/script.js
@@ -1,9 +1,16 @@
+// mostrar el icono de los textos del menú
+const icon = document.querySelector(".nav__circle");
+const txt = document.querySelector(".nav__txt");
+
+txt.addEventListener("click",() =>{
+
+    icon.classList.add("active");
+});
 
 
 // menú de navegación
 const open = document.querySelector(".nav__open");
 const close = document.querySelector(".nav__close");
-
 const nav = document.querySelector("nav");
 
 
